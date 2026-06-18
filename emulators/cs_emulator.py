fb357b25580f1b9e8e971c086f8033f80c205b53
yogihybo <smith.caleb@gmail.com>
2026-06-18 20:05:44 +1000

    fix emulator for consists

--- emulators/cs_emulator.py
+++ emulators/cs_emulator.py
@@ -1,7 +1,5 @@
 import socket
-import random
 import re
-import time
 from threading import Thread
 
 sockets = []
@@ -13,77 +11,105 @@ def broadcast(msg):
         except Exception:
             sockets.remove(s)
 
+def split_commands(raw: str) -> list[str]:
+    """Split a raw DCC-EX stream into individual <...> commands."""
+    return re.findall(r'<[^>]+>', raw)
+
+def handle_cmd(c: socket.socket, cmd: str):
+    if len(cmd) < 2:
+        c.send(b"<X>\n")
+        return
+
+    print("<< " + cmd)
+
+    match cmd[1]:
+        case "0":
+            if len(cmd) > 3 and cmd[3] == "M":
+                c.send(b"<p0 MAIN>\n")
+            elif len(cmd) > 3 and cmd[3] == "P":
+                c.send(b"<p0 PROG>\n")
+            else:
+                c.send(b"<p0>\n")
+
+        case "1":
+            if len(cmd) > 3 and cmd[3] == "M":
+                c.send(b"<p1 MAIN>\n")
+            elif len(cmd) > 3 and cmd[3] == "P":
+                c.send(b"<p1 PROG>\n")
+            elif len(cmd) > 3 and cmd[3] == "J":
+                c.send(b"<p1 JOIN>\n")
+            else:
+                c.send(b"<p1>\n")
+
+        case "s":
+            c.send(b"<iDCC-EX V-5.2.76 / MEGA / STANDARD_MOTOR_SHIELD / 12345>\n")
+
+        case "W":
+            c.send(b"<w 4321>\n")
+
+        case "R":
+            c.send(b"<r 1423>\n")
+
+        case "B":
+            c.send(b"<r12345|32767|3 2 1 >\n")
+
+        case "F":
+            c.send(b"<l 1 1 146 7>\n")
+
+        case "l":
+            c.send(b"<l 1 1 0 0>\n")
+
+        case "t":
+            # <t ADDR SPEED DIR>
+            r = re.search(r"<t (?P<l>\d+) (?P<s>\d+) (?P<d>\d+)>", cmd)
+            if r:
+                speed_dir = (int(r.group("s")) + 1 & 0x7F) + int(r.group("d")) * 128
+                reply = f"<l {r.group('l')} 0 {speed_dir} 0>\n"
+                print(">> " + reply.strip())
+                broadcast(reply.encode())
+            else:
+                c.send(b"<l 1 1 0 1>\n")
+
+        case "U":
+            c.send(b"<p1 MAIN>\n")
+
+        case "^":
+            # CS consist: <^ addr1 [-]addr2 ...> = create/update, <^ addr> = delete
+            inner = cmd[3:-1].strip()
+            parts = inner.split()
+            if len(parts) >= 2:
+                # Echo back as CS confirmation so the library processes the consist
+                reply = cmd + "\n"
+                print(">> " + cmd)
+                broadcast(reply.encode())
+            elif len(parts) == 1:
+                c.send(b"<O>\n")
+            else:
+                c.send(b"<X>\n")
+
+        case _:
+            c.send(b"<X>\n")
+
 def throttle(c: socket.socket):
     sockets.append(c)
+    buf = ""
 
     while True:
         data = c.recv(1024)
         if not data:
             c.close()
-            sockets.remove(c)
+            if c in sockets:
+                sockets.remove(c)
             break
 
-        cmd = data.decode().strip()
-        print("<< " + cmd)
+        buf += data.decode(errors="replace")
+        cmds = split_commands(buf)
+        # Trim processed commands from buffer; keep any partial tail
+        last = buf.rfind('>')
+        buf = buf[last + 1:] if last >= 0 else buf
 
-        if len(cmd) < 2:
-            c.send(b"<X>")
-            continue
-
-        match cmd[1]:
-            case "0":
-                if len(cmd) > 3 and cmd[3] == "M":
-                    c.send(b"<p0 MAIN>\n")
-                elif len(cmd) > 3 and cmd[3] == "P":
-                    c.send(b"<p0 PROG>\n")
-                else:
-                    c.send(b"<p0>\n")
-
-            case "1":
-                if len(cmd) > 3 and cmd[3] == "M":
-                    c.send(b"<p1 MAIN>\n")
-                elif len(cmd) > 3 and cmd[3] == "P":
-                    c.send(b"<p1 PROG>\n")
-                elif len(cmd) > 3 and cmd[3] == "J":
-                    c.send(b"<p1 JOIN>\n")
-                else:
-                    c.send(b"<p1>\n")
-
-            case "s":
-                # Match TeeStream parser: "iDCC-EX V-x.x.x / BOARD / SHIELD / BUILD"
-                c.send(b"<iDCC-EX V-5.2.76 / MEGA / STANDARD_MOTOR_SHIELD / 12345>\n")
-
-            case "W":
-                c.send(b"<w 4321>\n")
-
-            case "R":
-                c.send(b"<r 1423>\n")
-
-            case "B":
-                c.send(b"<r12345|32767|3 2 1 >\n")
-
-            case "F":
-                c.send(b"<l 1 1 146 7>\n")
-
-            case "l":
-                c.send(b"<l 1 1 0 0>\n")
-
-            case "t":
-                # Format: <t ADDR SPEED DIR>
-                r = re.search(r"<t (?P<l>\d+) (?P<s>\d+) (?P<d>\d+)>", cmd)
-                if r:
-                    speed_dir = (int(r.group("s")) + 1 & 0x7F) + int(r.group("d")) * 128
-                    reply = f"<l {r.group('l')} 0 {speed_dir} 0>\n"
-                    print(">> " + reply)
-                    broadcast(reply.encode())
-                else:
-                    c.send(b"<l 1 1 0 1>\n")
-
-            case "U":
-                c.send(b"<p1 MAIN>\n")
-
-            case _:
-                c.send(b"<X>\n")
+        for cmd in cmds:
+            handle_cmd(c, cmd)
 
 
 s = socket.socket()
